2803207afd3ec9bc7120781bbe376d8727709bb5
Malgo <gocha1@windowslive.com>
2024-01-20 14:36:15 +0100

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log("Cześć!");
+
+let button = document.querySelector(".button");
+let body = document.querySelector(".body");
+let themeName = document.querySelector(".themeName");
+
+
+button.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+        themeName.innerText = "jasny";
+    } else {
+        themeName.innerText = "ciemny";
+    }
+})
