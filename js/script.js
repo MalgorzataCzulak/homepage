29ef4ff043afad4b9c17d351f6cebcc6b6f0e046
Malgo <gocha1@windowslive.com>
2024-02-04 15:23:23 +0100

    function added 1

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
-console.log("Cześć! Ten kod jest już w reozytorium GIT");
-
-let button = document.querySelector(".button");
-let body = document.querySelector(".body");
-let themeName = document.querySelector(".themeName");
+function hello() {
+    console.log("Cześć! Ten kod jest już w reozytorium GIT");
+}
 
 
-button.addEventListener("click", () => {
+let backgroundToggle = () => {
     body.classList.toggle("dark");
+    article.classList.toggle("article--dark");
 
     if (body.classList.contains("dark")) {
         themeName.innerText = "jasny";
     } else {
         themeName.innerText = "ciemny";
     }
-})
+};
+
+hello ();
+ 
+
+
+
+let button = document.querySelector(".button");
+let body = document.querySelector(".body");
+let themeName = document.querySelector(".themeName");
+let article = document.querySelector(".article");
+
+
+button.addEventListener("click", backgroundToggle);
